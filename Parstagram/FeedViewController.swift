d6e91bb1dc5be5cdb453ebf1142ec0f269a1aaa8
Perla Escano <perlaescanoe@gmail.com>
2021-03-20 02:36:20 -0400

    are comment

--- Parstagram/FeedViewController.swift
+++ Parstagram/FeedViewController.swift
@@ -11,26 +11,38 @@ import AlamofireImage
 import MessageInputBar
 
 class FeedViewController: UIViewController, UITableViewDelegate,
-                          UITableViewDataSource {
+                          UITableViewDataSource, MessageInputBarDelegate {
    
 
     @IBOutlet weak var tableView: UITableView!
     
     let commentBar = MessageInputBar()
-    
+    var showsCommentBar = false
     var posts =  [PFObject]()
-    
+    var selectedPost: PFObject!
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        commentBar.inputTextView.placeholder = "Add a comment..."
+        commentBar.sendButton.title = "Post"
+        commentBar.delegate = self
+        
+        
         
         tableView.delegate = self
         tableView.dataSource = self
         
         
-        table.View.keyboardDismissMode = .interactive
-        // Do any additional setup after loading the view.
+        tableView.keyboardDismissMode = .interactive
+       
+        let center = NotificationCenter.default
+        center.addObserver(observer: self, selector: #selector(keyboardWillBeHidden(note:)), name: NSNotification.Name?, object: Any?)
     }
-    
+   
+    @objc func keyboardWillBeHidden(note: Notification) {
+        commentBar.inputTextView.text = nil
+        becomeFirstResponder()
+    }
     
     override var inputAccessoryView: UIView? {
         return commentBar
@@ -67,6 +79,8 @@ class FeedViewController: UIViewController, UITableViewDelegate,
         return posts.count
     }
     
+  
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
        
         let post = posts[indexPath.section]
@@ -100,23 +114,47 @@ class FeedViewController: UIViewController, UITableViewDelegate,
         
     }
     
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let post =  posts[indexPath.row]
-        
+    func messageInputBar(_ inputBar: MessageInputBar, didPressSendButtonWith text: String) {
+        //create the comment
         let comment = PFObject(className: "Comments")
-        comment["text"] = "This is a random comment"
-        comment["post"] = post
-        comment["author"] = PFUser.current()!
+         comment["text"] = text
+         comment["post"] = selectedPost
+         comment["author"] = PFUser.current()!
         
-        post.add(comment, forKey: "comments")
-        post.saveInBackground {(success, error)
+        SelectedPost.add(comment, forKey: "comments")
+        SelectedPost.saveInBackground {(success, error)
             in if success {
                 print("Comment saved")
-            } else {
-                print("Error saving comment")
-            }
+                } else {
+                        print("Error saving comment")
+                    }
+                }
+        tableView.reloadData()
+
+        
+        
+        //Clear and dissmiss input bar
+        commentBar.inputTextView.text = nil
+        
+        showsCommentBar = false
+        becomeFirstResponder()
+        commentBar.inputTextView.resignFirstResponder()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let post =  posts[indexPath.section]
+        let comment = (post["comments"] as? [PDObject]) ?? []
+        
+        if indexPath.row == comments.count + 1 {
+            showsCommentBar = true
+            becomeFirstResponder()
+            commentBar.inputTextView.becomeFirstResponder()
+            selectedPost = post
+          
         }
+        
+        
+        
     }
     
 
--- Podfile
+++ Podfile
@@ -1,24 +1,23 @@
 # Uncomment the next line to define a global platform for your project
 # platform :ios, '9.0'
 
-# NOTE: Change the string below to the name of your project
 target 'Parstagram' do
-  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
   # Pods for Parstagram
     pod "Parse"
     pod "AlamofireImage"
-    pod "MessageInputBar"
+    pod "MessageInputBar" 
     
   target 'ParstagramTests' do 
     inherit! :search_paths
     # Pods for testing
-   end
+     end
 
-  target 'ParstagramUITests' do=
+  target 'ParstagramUITests' do
     inherit! :search_paths
     # Pods for testing
    end
-
 end
+
